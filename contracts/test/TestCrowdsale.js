e6c2c6f4e4eae9b402de40c3755d82d4ea76eff6
Mukesh <mkumar.dion@gmail.com>
2018-05-11 10:16:00 +0530

    testcases

--- contracts/test/TestCrowdsale.js
+++ contracts/test/TestCrowdsale.js
@@ -1,36 +1,117 @@
-var ComedyplayToken = artifacts.require("ComedyplayToken");
+// contract to be tested
+var ComedyplayCrowdsale = artifacts.require("./ComedyplayCrowdsale");
 
-contract('ComedyplayToken', function(accounts) {
-  it('should deploy the token and store the address', function(done){
-        ComedyplayToken.deployed().then(async function(instance) {
-            assert(instance, 'Token address couldn\'t be stored');
-            done();
-       });
+
+// Test suite
+contract('ComedyplayCrowdsale', function (accounts) {
+    var contractInstance;
+    var owner = accounts[0];
+    var whitelistUser = accounts[1];
+
+    // Test case: Total supply fo the tokens
+    it("Total supply of the tokens", function () {
+        return ComedyplayCrowdsale.deployed().then(function (instance) {
+            contractInstance = instance;
+            return contractInstance.totalSupply();
+        }).then(function (data) {
+            assert.equal(web3.fromWei(data), 700000000, "Total supply of the tokens should be 700000000");
+        });
     });
 
-  it('one ETH should buy 20000 Comedyplay Tokens in Privatesale', function(done){
-      ComedyplayToken.deployed().then(async function(instance) {
-          const data = await instance.sendTransaction({ from: accounts[7], value: web3.toWei(1, "ether")});
-          const comedyplayToken = ComedyplayToken.at(instance);
-          const tokenAmount = await comedyplayToken.balanceOf(accounts[7]);
-          assert.equal(tokenAmount.toNumber(), 20000000000000000000000, 'The sender didn\'t receive the tokens as per Privatesale rate');
-          done();
-     });
-  });
+    // Test case: check current rate of the tokens
+    it("Current rate of the tokens", function () {
+        return ComedyplayCrowdsale.deployed().then(function (instance) {
+            contractInstance = instance;
+            return contractInstance.rate();
+        }).then(function (data) {
+            assert.equal(data.toNumber(), 20000, "Current rate of the tokens should be 20000");
+        });
+    });
 
-  it('should transfer the ETH to wallet immediately', function(done){
-        ComedyplayToken.deployed().then(async function(instance) {
-            let balanceOfBeneficiary = await web3.eth.getBalance(accounts[9]);
-            balanceOfBeneficiary = Number(balanceOfBeneficiary.toString(10));
+    // Test case: private sale period
+    it("private sale period", function () {
+        return ComedyplayCrowdsale.deployed().then(function (instance) {
+            contractInstance = instance;
+            return contractInstance.isPrivatesalePeriod();
+        }).then(function (data) {
+            assert.equal(data, true, "private sale period should be true");
+        });
+    });
 
-            await instance.sendTransaction({ from: accounts[1], value: web3.toWei(1, "ether")});
+    // Test case: private sale period
+    it("pre sale period", function () {
+        return ComedyplayCrowdsale.deployed().then(function (instance) {
+            contractInstance = instance;
+            return contractInstance.isPresalePeriod();
+        }).then(function (data) {
+            assert.equal(data, false, "pre sale period should be false");
+        });
+    });
 
-            let newBalanceOfBeneficiary = await web3.eth.getBalance(accounts[9]);
-            newBalanceOfBeneficiary = Number(newBalanceOfBeneficiary.toString(10));
+    // Test case: private sale period
+    it("main sale period", function () {
+        return ComedyplayCrowdsale.deployed().then(function (instance) {
+            contractInstance = instance;
+            return contractInstance.isMainsalePeriod();
+        }).then(function (data) {
+            assert.equal(data, false, "main sale period should be false");
+        });
+    });
 
-            assert.equal(newBalanceOfBeneficiary, balanceOfBeneficiary + 20000000000000000000000, 'ETH couldn\'t be transferred to the beneficiary');
-            done();
-       });
+    // Test case: weiRaised in the private sale
+    it("weiRaised in the private sale", function () {
+        return ComedyplayCrowdsale.deployed().then(function (instance) {
+            contractInstance = instance;
+            return contractInstance.PrivatesaleWeiRaised();
+        }).then(function (data) {
+            assert.equal(data.toNumber(), 0, "weiRaised in the private sale should be 0");
+        });
+    });
+
+    // Test case: weiRaised in the pre sale
+    it("weiRaised in the pre sale", function () {
+        return ComedyplayCrowdsale.deployed().then(function (instance) {
+            contractInstance = instance;
+            return contractInstance.PresaleWeiRaised();
+        }).then(function (data) {
+            assert.equal(data.toNumber(), 0, "weiRaised in the pre sale should be 0");
+        });
+    });
+
+    // Test case: weiRaised in the main sale
+    it("weiRaised in the main sale", function () {
+        return ComedyplayCrowdsale.deployed().then(function (instance) {
+            contractInstance = instance;
+            return contractInstance.mainsaleWeiRaised();
+        }).then(function (data) {
+            assert.equal(data.toNumber(), 0, "weiRaised in the main sale should be 0");
+        });
+    });
+
+    // Test case: token allocated
+    it("token allocated", function () {
+        return ComedyplayCrowdsale.deployed().then(function (instance) {
+            contractInstance = instance;
+            return contractInstance.tokenAllocated();
+        }).then(function (data) {
+            assert.equal(data.toNumber(), 0, "tokenAllocated should be 0");
+        });
+    });
+
+    // Test case: Only whitlist user can purchase the token
+    it("Only whitlist user can purchase the token", function () {
+        return ComedyplayCrowdsale.deployed().then(function (instance) {
+            contractInstance = instance;
+
+            return contractInstance.sendTransaction({
+                from: accounts[2],
+                value: web3.toWei(1, "ether"),
+                gas: 500000
+            });
+        }).then(assert.fail)
+            .catch(function (error) {
+                assert(error.message.indexOf('revert') >= 0, "error should be revert");
+            });
     });
 
 });
