460e9ebfe696789193decb0dfab527fc2776a8a8
Mukesh <mkumar.dion@gmail.com>
2018-05-22 10:10:53 +0530

    added code for web3 connection

--- contracts/migrations/2_ComedyplayCrowdsale.js
+++ contracts/migrations/2_ComedyplayCrowdsale.js
@@ -5,7 +5,7 @@ module.exports = function(deployer) {
   deployer.deploy(SafeMath);
   deployer.link(SafeMath, ComedyplayCrowdsale);
   deployer.deploy(ComedyplayCrowdsale,
-    "0x264F93F128Ff7a1F5813E593A2F2d4db6a709De7", // TODO : Update this address
-    "0x264F93F128Ff7a1F5813E593A2F2d4db6a709De7" // TODO : Update this address
+    "0x61A44075419C4402f6DE631341d875Ece6A3922e", // TODO : Update this address
+    "0x61A44075419C4402f6DE631341d875Ece6A3922e" // TODO : Update this address
     );
 };
